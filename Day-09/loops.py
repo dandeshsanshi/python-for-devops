28d5515187d0656799128f9d97020fc969a6fc79
Dandappa <dandappa.sanshi@publicissapient.com>
2024-12-31 11:55:06 +0530

    test

---
+++ Day-09/loops.py
@@ -0,0 +1,11 @@
+for i in range(1, 11):
+    print(i)
+
+fruits = ["apple", "banana", "cherry"]
+for fruit in fruits:
+    print(fruit)
+
+output = ""
+for i in range(1, 11):
+    output += f"{i} "
+    print(output)
